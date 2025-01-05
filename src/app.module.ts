140906dee3be1ed6d0683aafe3555062990ca9ca
sam-zarila <zarilatroxx99@gmail.com>
2025-01-05 14:42:53 -0800

    authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Authentication } from './authentication/entities/authentication.entity';
 
 @Module({
-  imports: [AuthenticationModule],
+  
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'Online_db',
+      entities: [Authentication],
+      synchronize: true,
+    }),
+    
+    AuthenticationModule],
   controllers: [AppController],
   providers: [AppService],
 })
